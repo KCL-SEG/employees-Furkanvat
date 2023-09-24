1b4d75acf0bfb1ea69b0f02cee9dd4b3ffcd7d7b
Furkanvat <117448902+Furkanvat@users.noreply.github.com>
2023-09-24 22:03:45 +0100

    Update employee.py

--- employee.py
+++ employee.py
@@ -2,30 +2,53 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+
+    def __init__(self, name, salaryType, bonusType, salary, contract, contractRate,bonus):
         self.name = name
+        self.salaryType = salaryType
+        self.bonusType = bonusType
+        self.salary = salary
+        self.contract = contract
+        self.contractRate = contractRate
+        self.bonus = bonus
+    
+    def get_bonus(self):
+        return self.contract * self.contractRate
+         
 
     def get_pay(self):
-        pass
+        if self.salaryType == "monthly":
+            if self.bonusType == "contractCommision":
+                self.salary += self.contract * self.contractRate
+            elif self.bonusType == "bonusCommision":
+                self.salary += self.bonus
+
+        elif self.salaryType == "contract":
+            if self.bonusType == "contractCommision":
+                self.salary += self.contract * self.contractRate
+            elif self.bonusType == "bonusCommision":
+                self.salary += self.bonus
+        
+        return self.salary
 
     def __str__(self):
         return self.name
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = Employee('Billie', "monthly", None, 4000, None , None, None )
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = Employee('Charlie', "contract", None, (100 * 25), None, None, None)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = Employee('Renee', "monthly", "contractCommision", 3000, 4, 200, None)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = Employee('Jan', "contract", "contractCommision", (150 * 25), 3, 220, None)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = Employee('Robbie', "monthly", "bonusCommision", 2000, None, None, 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = Employee('Ariel', "monthly", "bonusCommision", (120 * 30), None, None, 600)
